127348356d22ca40fef3c235057bfec732c21c0b
PanPrizrak <joker1992moi>
2018-08-21 09:02:19 +0300

    Пути генерируются от располажения рабочей папки

--- src/main/java/com/example/webfb/application/Application.java
+++ src/main/java/com/example/webfb/application/Application.java
@@ -1,33 +1,54 @@
 package com.example.webfb.application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.webfb.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
+    @Value("${upload.path}")
+    private static String uploadPath;
 
     public static void main(String[] args){
 
         SpringApplication.run(Application.class, args);
 
-        FileInputStream  fis;
-        Properties property = new Properties();
-
+        Properties prop = new Properties();
+        OutputStream output = null;
+        System.out.println(uploadPath);
         try {
-            fis = new FileInputStream("src/main/resources/application.properties");
-            property.load(fis);
-            property.setProperty("upload.path", "E://buf//web");
-        }catch(FileNotFoundException ex){
-            System.out.println(ex);
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            output = new FileOutputStream("src/main/resources/application.properties");
+            String resourcesPath = new File(".").getAbsolutePath()+"src/main/resources/uploads";
+            // set the properties value
+            prop.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+            prop.setProperty("spring.datasource.url", "jdbc:mysql://localhost:3306/db_example");
+            prop.setProperty("spring.datasource.username", "root");
+            prop.setProperty("spring.datasource.password", "root");
+
+            prop.setProperty("spring.freemarker.expose-request-attributes", "true");
+
+            prop.setProperty("upload.path", "/"+resourcesPath);
+
+
+            // save properties to project root folder
+            prop.store(output, null);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
     }
